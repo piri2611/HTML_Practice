6149b2a0097563a2580f8704773af3f8b8ebc902
Developer <developer@example.com>
2026-02-02 09:24:00 +0530

    Rebuild dist with correct compiled JavaScript files

--- dist/assets/app.js
+++
@@ -1,4 +0,0 @@
-// This is a placeholder for the compiled application
-// To generate this file, run: npm run build
-// The actual build will contain the complete compiled React application
-console.log('Quiz Application - Please build using npm run build');
